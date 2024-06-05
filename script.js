5126b374c158d5db822e93ab730da6720cbd22c0
therandrade <esthereduarda89@gmail.com>
2024-06-05 14:41:24 -0300

    terminado

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function calcular(){
-    var fPeso = parseFloat(document.getElementById("Peso"))
-    var fAltura = parseFloat(document.getElementById("Altura"))
-    var FIMC = fPeso/(fAltura*fAaltura)
-    
+function Calcular(){
+    var fPeso = parseFloat(document.getElementById("Peso").value)
+    var fAltura = parseFloat(document.getElementById("Altura").value)
+    var fIMC = fPeso/(fAltura*fAltura)
+    document.getElementById("IMC").innerHTML = fIMC.toFixed(2)
+    if(fIMC<18.5){
+        document.getElementById("Status").innerHTML = "Status: Baixo Peso"
+    }else if(fIMC<=24.9){
+        document.getElementById("Status").innerHTML = "Status: Peso Ideial"
+    }else if(fIMC<=29.9){
+        document.getElementById("Status").innerHTML = "Status: Sobrepeso"
+    }else if(fIMC<=34.9){
+        document.getElementById("Status").innerHTML = "Status: Obesidade 1"
+    }else if(fIMC<=39.9){
+        document.getElementById("Status").innerHTML = "Status: Obesidade 2"
+    }else{
+        document.getElementById("Status").innerHTML = "Status: Obesidade Morbida"   
+    }
 }
